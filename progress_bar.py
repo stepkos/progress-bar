78c8db39496bfdd6c895e25314971a70a7f86296
stepkos <stepkos@wp.pl>
2022-09-22 14:46:40 +0200

    Refactor code

--- progress_bar.py
+++ progress_bar.py
@@ -1,9 +1,5 @@
 class ProgressBar:
 
-    hide_coursor = lambda self: print('\033[?25l', end='')
-    show_coursor = lambda self: print('\033[?25h', end='')
-
-
     def __init__(self, length: int = 20,
             _open: str = '[', close: str = ']',
             fill: str = '#', fill_other_first: str = '',
@@ -15,36 +11,44 @@ class ProgressBar:
         self.fill_other_first = fill_other_first
         self.loading_chars = loading_chars
 
-    
+
     def __enter__(self):
         self.hide_coursor()
         return self
 
 
-    def __exit__(self, *args, **kwargs):
+    def __exit__(self, *args):
         self.show_coursor()
 
-    
+
     def __getitem__(self, item):
         if item > self.length:
             raise StopIteration()
 
         line = '{load_char} {_open}{fill}{unfill}{close} {percent}%'.format(
-            load_char = self.loading_chars[item % len(self.loading_chars)],
-            _open = self.open,
-            fill = self.fill * (item - 1) + self.fill_other_first if self.fill_other_first else self.fill,
-            unfill = self.unfill * (self.length - item),
-            close = self.close,
-            percent = item * 100 // self.length
+            load_char=self.loading_chars[item % len(self.loading_chars)],
+            _open=self.open,
+            fill=self.fill * (item - 1) + self.fill_other_first if self.fill_other_first else self.fill,
+            unfill=self.unfill * (self.length - item),
+            close=self.close,
+            percent=item * 100 // self.length
         )
 
         print(line, end='\r')
-        
+
+
+    def hide_coursor(self): 
+        print('\033[?25l', end='')
+
+
+    def show_coursor(self):
+        print('\033[?25h', end='')
+
 
 
 if __name__ == '__main__':
     import time
-    
+
     with ProgressBar(fill='=', fill_other_first='>', unfill='.') as pb:
         for i in pb:
             time.sleep(.2)
